1bf65c4d92c134c7537c522e8a6f8ef191fd9499
Collin VanDyck <gluedtomyseat@gmail.com>
2008-10-06 17:27:38 -0400

    Updated some admin controllers to enforce auth

--- app/controllers/admin/resets_controller.rb
+++ app/controllers/admin/resets_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ResetsController < ApplicationController
   
+  before_filter :requires_login
+  
   def create
     PhraseCount.destroy_all
     redirect_to root_path
--- app/controllers/admin/updates_controller.rb
+++ app/controllers/admin/updates_controller.rb
@@ -1,5 +1,7 @@
 class Admin::UpdatesController < ApplicationController
   
+  before_filter :requires_login
+  
   def create
     Page.count_all
     redirect_to root_path
