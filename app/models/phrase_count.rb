b26d7312bce949141ce9aef7239ca10300ff7218
Collin VanDyck <gluedtomyseat@gmail.com>
2008-10-14 08:43:22 -0400

    Phrase counts should be ordered in reverse chronological order

--- app/models/phrase_count.rb
+++ app/models/phrase_count.rb
@@ -2,7 +2,7 @@ class PhraseCount < ActiveRecord::Base
 
   named_scope :for_phrase, lambda { |phrase| { :include => :phrase, :conditions => ["phrases.text = ?", phrase]}}
   named_scope :in_range, lambda { |from, to| { :conditions => ["phrase_counts.created_at >= ? and phrase_counts.created_at <= ?", from, to]}}
-  named_scope :ordered, :order => "phrase_counts.created_at"
+  named_scope :ordered, :order => "phrase_counts.created_at DESC"
   named_scope :including_phrases, :include => :phrase
   named_scope :limited_to, lambda { |limit| { :limit => limit }}
   
